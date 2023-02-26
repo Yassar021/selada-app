72af2d97c49f82ad192dd538a0a3d7d5c82ae6f5
Nursan amar <nursan011@gmail.com>
2023-02-26 20:40:18 +0800

    add confident value

--- src/components/Diagnosa/index.jsx
+++ src/components/Diagnosa/index.jsx
@@ -97,7 +97,7 @@ const Diagnosa = () => {
                         <Flex direction='column' gap='20px'>
                             <Text>Hasil Diagnosa</Text>
 
-                            <Content img={imageUrl} title={result?.prediction.lable} subtitle={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."} />
+                            <Content confident={result?.prediction.confident} img={imageUrl} title={result?.prediction.lable} subtitle={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."} />
                         </Flex>
                     </Container>
             }
--- src/components/Informasi/content.jsx
+++ src/components/Informasi/content.jsx
@@ -1,11 +1,12 @@
 import { Flex, Text } from "@chakra-ui/react"
 import Image from "next/image"
 
-const Content = ({ img, title, subtitle }) => {
+const Content = ({ img, title, subtitle, confident }) => {
     return (
         <Flex direction="column" gap='10px'>
             <Image width="250" height="250" src={img} alt="content" />
             <Text fontSize={"16px"}>{title}</Text>
+            <Text fontSize={"14px"}>{`Confident: ${confident}`}</Text>
             <Text fontSize={"12px"}>{subtitle}</Text>
         </Flex>
 
